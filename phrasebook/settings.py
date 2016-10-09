b06d74632ff621c5c83bb16ee73d1c6d03e7f2fb
michaelsnook <michael.snook@gmail.com>
2016-10-09 20:38:16 +0530

    Load database from a single URL var with dj-database-url

--- phrasebook/settings.py
+++ phrasebook/settings.py
@@ -81,13 +81,15 @@ WSGI_APPLICATION = 'phrasebook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'phrasebook',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
