7e398bce17bfc95471e49d95b00d396049eae7df
michaelsnook <michael.snook@gmail.com>
2016-10-12 20:33:50 +0530

    Add default postgres connection

--- phrasebook/settings.py
+++ phrasebook/settings.py
@@ -86,6 +86,17 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'phrasebook',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
